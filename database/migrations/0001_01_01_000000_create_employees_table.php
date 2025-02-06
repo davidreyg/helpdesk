79307bf3014b1d08fa449dd52ccef2b44de29536
David Rey <neisserrey@upeu.edu.pe>
2025-02-06 04:36:59 +0000

    feat: :boom: SuperAdmin
    Add employee_id to users table

--- database/migrations/0001_01_01_000000_create_employees_table.php
+++ database/migrations/0001_01_01_000000_create_employees_table.php
@@ -15,11 +15,11 @@ return new class extends Migration {
             $table->string('names', 200);
             $table->string('paternal_surname', 200);
             $table->string('maternal_surname', 200);
-            $table->string('email', 100);
+            $table->string('email', 100)->unique();
             $table->integer('phone')->unsigned();
             $table->date('birth_date');
             $table->string('document_type', 100);
-            $table->integer('document_number')->unsigned();
+            $table->integer('document_number')->unique()->unsigned();
             $table->string('gender', 100);
             $table->string('address', 100)->nullable();
             $table->timestamps();
---
+++ database/seeders/SuperAdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Employee;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SuperAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'superadmin',
+            'email' => 'superadmin@superadmin.com',
+            'email_verified_at' => now(),
+            'password' => \Hash::make('password'),
+            'remember_token' => \Str::random(10),
+            'employee_id' => Employee::first()->id,
+        ]);
+    }
+}
