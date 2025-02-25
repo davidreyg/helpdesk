271bec4a1801aaa81657e0dd8b26eb3f3fc0d378
David Rey <neisserrey@upeu.edu.pe>
2025-02-25 03:41:45 +0000

    fix: :adhesive_bandage: fix logo path

--- database/settings/2024_02_20_131300_create_general_settings.php
+++ database/settings/2024_02_20_131300_create_general_settings.php
@@ -7,9 +7,9 @@ return new class extends SettingsMigration {
     public function up(): void
     {
         $this->migrator->add('general.brand_name', 'Sistema de Gestion Incidentes');
-        $this->migrator->add('general.brand_logo', 'images/logo.jpg');
+        $this->migrator->add('general.brand_logo', 'sites/logo.jpg');
         $this->migrator->add('general.brand_logoHeight', '3rem');
         $this->migrator->add('general.site_active', true);
-        $this->migrator->add('general.site_favicon', 'favicon.ico');
+        $this->migrator->add('general.site_favicon', 'sites/favicon.ico');
     }
 };
