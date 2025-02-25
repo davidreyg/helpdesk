d40e018873be05277f39a452cf983400ea93f6ad
David Rey <neisserrey@upeu.edu.pe>
2025-02-25 05:13:01 +0000

    fix: :ambulance: Reverb

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,19 @@
 use App\Actions\PdfGenerator;
 use App\Enums\ReportTypeEnum;
 use App\Models\Quotation;
+use Filament\Notifications\Notification;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/test', function () {
+    $recipient = auth()->user();
+
+    Notification::make()
+        ->title('Saved successfully')
+        ->success()
+        ->broadcast($recipient);
 });
 
 Route::get('/pdf/quotations/{quotation}', function (Quotation $quotation) {
