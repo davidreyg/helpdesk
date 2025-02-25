f13c712e8224ef7e350e027108ab94608492938c
David Rey <neisserrey@upeu.edu.pe>
2025-02-25 03:34:46 +0000

    fix: :bug: docker compose prod and reverb port

--- app/Http/Middleware/ConfigureCurrentPanel.php
+++ app/Http/Middleware/ConfigureCurrentPanel.php
@@ -2,11 +2,8 @@
 
 namespace App\Http\Middleware;
 
-use App\Events\CompanyConfigured;
 use App\Events\PanelConfigured;
-use App\Models\Company;
 use Closure;
-use Filament\Facades\Filament;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
