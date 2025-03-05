e7eb8e3f5fb9908fdb658131a8f774c851699ddf
David Rey <neisserrey@upeu.edu.pe>
2025-03-05 05:29:50 +0000

    fix: :adhesive_bandage: email verification support

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,6 +29,7 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
+            ->emailVerification()
             ->id('admin')
             ->path('admin')
             ->login()
