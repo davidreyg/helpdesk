7ecb8e7e6c29916a8ded8a3261ed043c42b0eac9
David Rey <neisserrey@upeu.edu.pe>
2025-03-05 05:24:58 +0000

    fix: :adhesive_bandage: mail settings

--- database/settings/2024_02_21_153246_create_mail_settings.php
+++ database/settings/2024_02_21_153246_create_mail_settings.php
@@ -2,17 +2,16 @@
 
 use Spatie\LaravelSettings\Migrations\SettingsMigration;
 
-return new class extends SettingsMigration
-{
+return new class extends SettingsMigration {
     public function up(): void
     {
-        $this->migrator->add('mail.from_address', 'no-reply@starter-kit.com');
-        $this->migrator->add('mail.from_name', 'SuperDuper Starter Kit');
+        $this->migrator->add('mail.from_address', 'corporativo2@masterelectronics.pe');
+        $this->migrator->add('mail.from_name', 'Master Electronics');
         $this->migrator->add('mail.driver', 'smtp');
-        $this->migrator->add('mail.host', null);
-        $this->migrator->add('mail.port', 587);
+        $this->migrator->add('mail.host', 'mail.masterelectronics.pe');
+        $this->migrator->add('mail.port', 465);
         $this->migrator->add('mail.encryption', 'tls');
-        $this->migrator->addEncrypted('mail.username', null);
+        $this->migrator->addEncrypted('mail.username', 'corporativo2@masterelectronics.pe');
         $this->migrator->addEncrypted('mail.password', null);
         $this->migrator->add('mail.timeout', null);
         $this->migrator->add('mail.local_domain', null);
