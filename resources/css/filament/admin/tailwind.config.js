fd9a86cd4336f2acf442cd8631dad3acf2cd2043
David Rey <neisserrey@upeu.edu.pe>
2025-04-18 15:39:54 +0000

    fix: :art: Prettier styles

--- resources/css/filament/admin/tailwind.config.js
+++ resources/css/filament/admin/tailwind.config.js
@@ -1,16 +1,15 @@
-import preset from '../../../../vendor/filament/filament/tailwind.config.preset'
+import preset from '../../../../vendor/filament/filament/tailwind.config.preset';
 
 export default {
-    presets: [preset],
-    content: [
-        "./app/Filament/**/*.php",
-        "./resources/views/filament/**/*.blade.php",
-        "./resources/views/infolists/**/*.blade.php",
-        "./resources/views/components/forms/**/*.blade.php",
-        "./resources/views/components/pdf/**/*.blade.php",
-        './vendor/filament/**/*.blade.php',
-        "./vendor/statikbe/laravel-filament-chained-translation-manager/**/*.blade.php",
-        "./vendor/awcodes/filament-table-repeater/resources/**/*.blade.php",
-
-    ],
-}
+  presets: [preset],
+  content: [
+    './app/Filament/**/*.php',
+    './resources/views/filament/**/*.blade.php',
+    './resources/views/infolists/**/*.blade.php',
+    './resources/views/components/forms/**/*.blade.php',
+    './resources/views/components/pdf/**/*.blade.php',
+    './vendor/filament/**/*.blade.php',
+    './vendor/statikbe/laravel-filament-chained-translation-manager/**/*.blade.php',
+    './vendor/awcodes/filament-table-repeater/resources/**/*.blade.php',
+  ],
+};
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,23 @@
-import { defineConfig } from 'vite'
-import laravel, { refreshPaths } from 'laravel-vite-plugin'
+import laravel, { refreshPaths } from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/filament/admin/theme.css'],
-            refresh: [
-                ...refreshPaths,
-                'app/Filament/**',
-                'app/Forms/Components/**',
-                'app/Livewire/**',
-                'app/Infolists/Components/**',
-                'app/Providers/Filament/**',
-                'app/Tables/Columns/**',
-            ],
-        }),
-    ],
-})
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+        'resources/css/filament/admin/theme.css',
+      ],
+      refresh: [
+        ...refreshPaths,
+        'app/Filament/**',
+        'app/Forms/Components/**',
+        'app/Livewire/**',
+        'app/Infolists/Components/**',
+        'app/Providers/Filament/**',
+        'app/Tables/Columns/**',
+      ],
+    }),
+  ],
+});
