2f940483dda2cab93681c48df1d2dc6b380d8921
David Rey <neisserrey@upeu.edu.pe>
2025-08-02 12:43:52 -0500

    feat: :boom: Discount model

---
+++ database/migrations/2025_08_02_122710_create_discounts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('discounts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('quotation_id')->constrained()->cascadeOnDelete();
+            $table->string('type', 20);
+            $table->integer('amount')->unsigned();
+            $table->integer('value')->unsigned();
+            $table->text('description')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('discounts');
+    }
+};
